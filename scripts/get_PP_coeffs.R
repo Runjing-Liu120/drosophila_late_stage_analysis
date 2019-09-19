b07a79cc5e6b492ba42c9ab6ed5f5126842d958c
Runjing <runjing_liu@berkeley.edu>
2019-09-19 16:46:17 -0700

    save as rdata doesnt work ... save as csv instead

--- scripts/get_PP_coeffs.R
+++ scripts/get_PP_coeffs.R
@@ -7,7 +7,7 @@ results_dir <- './staNMFDicts/'
 
 args <- commandArgs(trailingOnly = TRUE)
 
-K <- as.integer(args[1])
+K <- 19 # as.integer(args[1])
 
 print(paste0('getting PP coeffs for fit with K = ', K))
 
@@ -29,18 +29,12 @@ get_PP_coeffs <- function(x, dict){
 }
 
 # load dictionary 
+# for now ... we pick any restart
 dict_file_k <- paste0(results_dir, 'K=', K, '/factorization_99.csv')
 dict <- read.csv(file = dict_file_k, header = FALSE)[, -1]
 
 alpha <- get_PP_coeffs(x, as.matrix(dict))
 
-
-fit <- list(x = x, 
-          gene.names = colnames(x), 
-          alpha = alpha, 
-          dict = dict, 
-          template = late$template)
-
-outfile <- paste0('../data/late_K', K, '.Rdata')
+outfile <- paste0(results_dir, 'K=', K, '/alpha_99.csv')
 print(paste0('saving fit into: ', outfile))
-save(fit, file = outfile)
+write.csv(alpha, file = outfile)
