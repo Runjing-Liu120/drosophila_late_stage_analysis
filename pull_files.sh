8daa3893f98eccf9ca4b517916cfe5df1600917c
runjing_liu <runjing_liu@berkeley.edu>
2021-06-17 14:37:03 -0700

    reading in results

--- pull_files.sh
+++ pull_files.sh
@@ -1,6 +1,6 @@
 rsync -avz \
-   runjing_liu@arwen.berkeley.edu:/accounts/grad/runjing_liu/Drosophila/drosophila_late_stage_analysis/scripts/irf_fits_2020-04-13/. \
-   /home/rliu/Documents/Drosophila/late_stage_analysis/scripts/irf_fits_2020-04-13/. \
+   runjing_liu@arwen.berkeley.edu:/accounts/grad/runjing_liu/Drosophila/drosophila_late_stage_analysis/model_fits/. \
+   /home/rliu/Documents/Drosophila/late_stage_analysis/model_fits/. \
 
 
 
